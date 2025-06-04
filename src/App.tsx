95f7771bf6bcd0a9b67461df60331c718872854b
Martin Hoffmann <marthoff@gmail.com>
2025-06-04 10:51:43 +0200

    Icon

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
       <div className='flex flex-none'>
 
         <div className='p-2  text-gray-200'>
-          <h1 className='text-2xl font-bold'>Push Chord Grid</h1>
+          <h1 className='text-2xl font-bold'>Ableton Push Chord Grid</h1>
           <p className="app-description">
             This generates chords for Ableton Push. It is still in Alpha, so please check if the results are correct.
-            Feel free to report bugs on the <a className='' href='https://github.com/marthoff/push-chords' target='_blank' rel='noopener noreferrer'>
+            Feel free to report bugs on the <a className='text-blue-400' href='https://github.com/marthoff/push-chords' target='_blank' rel='noopener noreferrer'>
               GitHub Repository
             </a>.
           </p>
